8e13378fcb4090a4a9763d78c92671b071bd9d11
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-01-20 20:07:57 +0300

    Add files via upload
    first commentt

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static void Add(Musteriler m)
+        {
+            Console.WriteLine(m.FirstName + "  adlı musteri Eklendi!");
+        }
+        public static void Delete(Musteriler m)
+        {
+            Console.WriteLine(m.FirstName + "adlı musteri Silindi!");
+        }
+    }
+}
